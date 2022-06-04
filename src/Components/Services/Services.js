c7f26e56670dd4b9f82bf55ce39934ed1cfb6139
ahmed-nasir <nasirahmed595@gmail.com>
2022-06-04 14:15:03 +0600

    service section complete

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import './Services.css'
-import HeartEmoji from "..//../img/heartemoji.png"
-import Glasses from "..//../img/glasses.png"
-import Humble from "..//../img/humble.png"
+import HeartEmoji from "../../img/heartemoji.png"
+import Glasses from "../../img/glasses.png"
+import Humble from "../../img/humble.png"
+import Card from '../Card/Card'
+import Resume from './Resume.pdf'
 
 export const Services = () => {
     return (
@@ -12,16 +14,42 @@ export const Services = () => {
                 <span>My Awsome</span>
                 <span>services</span>
                 <span>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit consequuntur, iure dolore sapiente vero quidem voluptatum voluptas,
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit consequuntur,
                     <br />
-                    officiis assumenda nemo quam nam asperiores eos ratione ex similique sed libero modi!
+                    officiis assumenda eos ratione ex similique sed libero modi!
                 </span>
-                <button className='button s-button'>Download CV</button>
-                <div className='blur s-blur' style={{ background: "#ABF1FF94" }}></div>
+                <a href={Resume} download>
+                    <button className='button s-button'>Download CV</button>
+                </a>
+                <div className='blur s-blur1' style={{ background: "#ABF1FF94" }}></div>
             </div>
             {/* Right side */}
             <div className="cards">
-                I am the right
+                {/* 1st */}
+                <div style={{ left: '14rem' }}>
+                    <Card
+                        emoji={HeartEmoji}
+                        heading={'Design'}
+                        detail={'Figma, Sketch, Photoshop, Adobe, Adobe Xd'}
+                    />
+                </div>
+                {/* 2nd */}
+                <div style={{ top: '12rem', left: '-4rem' }}>
+                    <Card
+                        emoji={Glasses}
+                        heading={'Developer'}
+                        detail={'Html, Css, Javascript, React'}
+                    />
+                </div>
+                {/* 3rd */}
+                <div style={{ top: '19rem', left: '12rem' }}>
+                    <Card
+                        emoji={Humble}
+                        heading={'UI/Ux'}
+                        detail={'Figma, Sketch, Photoshop, Adobe, Adobe Xd'}
+                    />
+                </div>
+                <div className='blur s-blur2' style={{ background: "var(--purple)" }}></div>
             </div>
         </div>
     )
